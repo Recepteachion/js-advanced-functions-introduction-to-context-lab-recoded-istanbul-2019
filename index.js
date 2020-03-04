2dc12b47a6ca2917677091d60b859a513fbca1f2
Recep Öztürk <turkishteacher23@gmail.com>
2020-03-04 12:24:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Your code here
-function createEmployeeRecord(array=[str,str,str,num]){
-  return {
+
+function createEmployeeRecord(array) {
+  let arr = {
+
     firstName :array[0],
     familyName:array[1],
     title:array[2],
@@ -8,55 +10,75 @@ function createEmployeeRecord(array=[str,str,str,num]){
     timeInEvents:[],
     timeOutEvents:[]
   }
-  
-  
+  return arr
 }
 
 function createEmployeeRecords(arrayOfArrays){
   return arrayOfArrays.map(el => createEmployeeRecord(el))
 }
 
-function createTimeInEvent( obj = createEmployeeRecords(array),dt="2014-02-28 1400"){
+function createTimeInEvent( obj ,dt="2014-02-28 14:00"){
   
   let date = new Date(dt)
   obj.timeInEvents.push({
     type:"TimeIn",
-    hour :date.getHours(),
+    hour :date.toLocaleDateString().split("/").reverse().join("-") ,
     date :date.getDate()
   })
   
-  return obj.timeInEvents
+  return obj
 }
 
-function createTimeOutEvent( obj = createEmployeeRecords(array),dt="2014-02-28 1400"){
-  
+function createTimeOutEvent( obj ,dt){
   let date = new Date(dt)
-  obj.timeOutEvents.push({
+  obj.timeInEvents.push({
     type:"TimeIn",
-    hour :date.getHours(),
+    hour :datetoLocaleDateString().split("/").reverse().join("-") ,
     date :date.getDate()
   })
   
-  return obj.timeOutEvents
+  return obj
 }
   
-  
 
 
-function hoursWorkedOnDate(obj =createEmployeeRecords(array),date = new Date("2014-02-28")){
-     
-   return parseInt(obj.timeOutEvents[hour]- obj.timeInEvents[hour])
+function hoursWorkedOnDate(obj ,dt="2014-02-28"){
+     let date = new Date("2014-02-28")
+     return parseInt(obj.timeOutEvents[date]- obj.timeInEvents[date])
 }
 
 
 function wagesEarnedOnDate(obj =createEmployeeRecords(array),date = new Date("2014-02-28")){
    
-   return hoursWorkedOnDate()*obj[payPerHour]
+  return hoursWorkedOnDate()*obj[payPerHour]
 }
 
 
 function allWagesFor(obj =createEmployeeRecords(array)){
   return wagesEarnedOnDate()
+
+ 
+  
+}
+
+function createEmployeeRecords(array){
+  return array.map(el => createEmployeeRecord(el))
+}
+
+
+
+function hoursWorkedOnDate(array,dt){
+  let obj =createEmployeeRecord(array) 
+  let date = new Date(dt)
+}
+
+function wagesEarnedOnDate(array,dt){
+  let obj =createEmployeeRecord(array) 
+  let date = new Date(dt)
+}
+function allWagesFor(array){
+  
+
 }
 
 function findEmployeeByFirstName(srcArray,firstName){
@@ -65,6 +87,11 @@ function findEmployeeByFirstName(srcArray,firstName){
   
 }
 
+
 function calculatePayroll(arr=createEmployeeRecords(array)){
-  return wagesEarnedOnDate()
+  return wagesEarnedOnDate()}
+
+function calculatePayroll(array){
+  return array
+
 }
